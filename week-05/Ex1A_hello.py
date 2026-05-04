6323766826f2d0149a03e7bee893d51eb069c350
Marenza Santarin <renziesantarin@gmail.com>
2026-05-04 16:12:16 -0400

    Completed Ex1A exercise

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,29 @@
+# Marenza Santarin
+# 5/4/2026
+
+
 print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+# It prints twice because the first print statement prints the text directly,
+# the second print statement prints the same text stored in the variable.
+
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = 0.50
+print(dollars + cents)
+
+# The result shows 3.5 instead of 3.50 because Python is treating it
+# like a number and not money formatting.
+
+cents = cents + 0.25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str + " " + c_str)
