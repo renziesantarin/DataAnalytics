33fe72d6a844c73bfcb2dcd03cb48cecbd683151
Marenza Santarin <renziesantarin@gmail.com>
2026-05-05 03:40:30 -0400

    Completed Ex1B exercise

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,23 @@
+# Marenza Santarin
+# 5/5/2026
+
+customer_id = 101 # I used a number because IDs are often numeric.
+customer_name = "Bumi Santarin" # I used first and last name together, but it could also be separated into first_name and last_name.
+customer_gender = "Female" # I used text, but the company may use codes like F, M, X, or prefer not to answer.
+customer_date_of_birth = "2023-05-01" # I used a string because dates can be stored in different formats.
+drivers_license_number = "B1234567" # I used a string because license numbers can contain both letters and numbers.
+auto_policy_number = "APN-123456" # I used a string because policy numbers may contain letters, numbers, and symbols.
+
+my_name = "Marenza Santarin"
+hometown = "Imus, City"
+
+# a. What is the full list of reserved words that can’t be used for variable names?
+# False, None, True, and, as, assert, async, await, break, class, continue, def, del, elif, else, except, finally, for, from,
+# global, if, import, in, is, lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield
+
+# b. Pick 5 of these words and review the explanation for how it is used as a keyword in Python.
+# None: Represents the absence of a value.
+# True: A boolean operator that means the value is True.
+# False: A boolean operator that means the opposite of True.
+# if: Used to make a conditional statement.
+# elif: Checks if some other condition holds when the condition in the if statement is false.
