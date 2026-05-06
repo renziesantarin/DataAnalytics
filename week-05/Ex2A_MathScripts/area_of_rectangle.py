3a7e6cbe4e8c32ed3e7f43baa62e60d15242460c
Marenza Santarin <renziesantarin@gmail.com>
2026-05-06 12:04:48 -0400

    Added Ex2A area of rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,11 @@
+# Area of rectangle:
+# Area = length x width
+
+side_a = 3 # month
+side_b = 22 # day
+
+area = side_a * side_b
+
+print(f"Side A is {side_a}.")
+print(f"Side B is {side_b}.")
+print(f"The area of the rectangle is {area}.")
