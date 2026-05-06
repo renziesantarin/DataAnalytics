7c4bded27252d6551e0f5ab8b7bb55cc02084146
Marenza Santarin <renziesantarin@gmail.com>
2026-05-06 12:11:10 -0400

    Added Ex2A tip amount

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,10 @@
+# Tip formula:
+# Tip = bill x (percentage / 100)
+
+bill = 70
+tip_percent = 20
+
+# Tip calculation
+tip = bill * (tip_percent / 100)
+
+print(f"The tip on a ${bill:,.2f} restaurant bill is ${tip:,.2f}")
