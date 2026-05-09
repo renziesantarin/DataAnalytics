024302aac16735309cf6fdc8586430b3f5f90dca
Marenza Santarin <renziesantarin@gmail.com>
2026-05-09 02:20:14 -0400

    Added loop_love exercise

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,13 @@
+# Create list of words that means great
+words = ["amazing", "awesome", "excellent", "fantastic", "great"]
+
+# Loop through the list
+for word in words:
+
+    print(f"Loops are {word}!")
+# the variable word will store one item at a time from the list
+# until it gets to the end of the list. And then the loop will
+# stop and move on to the next line of code.
+
+# Print final message
+print("I <3 loops")
